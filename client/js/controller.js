823459abdd99939590cf1e3fae0a200c29a2f9c8
Vincent Wochnik <v.wochnik@gmail.com>
2014-10-13 11:35:50 +0200

    ctrl fix for list

--- client/js/controller.js
+++ client/js/controller.js
@@ -3,7 +3,9 @@ function AppCtrl ($scope) {
     $scope.title = 'The Movie Database';
 }
 
-function WelcomeCtrl () {
+function WelcomeCtrl (moviesResponse) {
+    'use strict';
+    $scope.movies = moviesResponse.data;
 }
 
 function MoviesListCtrl ($scope, $location, moviesResponse) {
@@ -14,7 +16,7 @@ function MoviesListCtrl ($scope, $location, moviesResponse) {
     };
 }
 
-MoviesListCtrl.resolve = {
+MoviesListCtrl.resolve = WelcomeCtrl.resolve = {
     moviesResponse: function ($http) {
         'use strict';
         return $http.get('/movies');
